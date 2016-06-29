4417cef68452b6a9b5a358a519ca37990844de3c
Davide Bettio <davide@uninstall.it>
2016-06-29 21:21:06 +0200

    bson.h: Add doxygen documentation

--- src/bson.h
+++ src/bson.h
@@ -1,3 +1,8 @@
+/**
+ * @file bson.h
+ * @brief a set of useful functions to read BSON documents
+ */
+
 /***************************************************************************
  *   Copyright 2016 by Davide Bettio <davide@uninstall.it>                 *
  *                                                                         *
@@ -26,9 +31,63 @@
 #define TYPE_STRING 0x02
 #define TYPE_INT32 0x10
 
+/**
+ * @brief lookup a certain key in a BSON document.
+ *
+ * @details finds a pointer to a value in a BSON object using a certain property key.
+ *
+ * @param key the property key used to perform the lookup.
+ * @param document a pointer to a BSON document/object.
+ * @param found value type (e.g. double, string, int32, etc...).
+ *
+ * @return a pointer to the value fond inside the document/object.
+ */
 const void *bson_key_lookup(const char *key, const void *document, uint8_t *type);
+
+/**
+ * @brief obtains the C string pointer to the string which valuePtr points to.
+ *
+ * @details this function should be used to get the C string that valuePtr points to.
+ * If valuePtr doesn't point to a C string a NULL value will be returned.
+ * If len parameter is not NULL it will be updated with the lenght of the returned
+ * string.
+ * This function doesn't return a copy, so the validity of that pointer has the same
+ * lifecycle of the BSON document.
+ *
+ * @param valuePtr the pointer to the value
+ * @param len the optional pointer to the variable that it may contain string len.
+ *
+ * @return a pointer to the C string that valuePtr points to, NULL if valuePtr doesn't
+ * point to a string
+ */
 const char *bson_value_to_string(const void *valuePtr, uint8_t *len);
+
+/**
+ * @brief obtains the C string pointer to the string which valuePtr points to.
+ *
+ * @details this function should be used to get the C string that valuePtr points to.
+ * If valuePtr doesn't point to a valid C string the function will return an invalid value.
+ * If len parameter is not NULL it will be updated with the lenght of the returned
+ * string.
+ * This function doesn't return a copy, so the validity of that pointer has the same
+ * lifecycle of the BSON document.
+ *
+ * @param valuePtr the pointer to the value.
+ * @param len the optional pointer to the variable that it may contain string len.
+ *
+ * @return a pointer to the C string that valuePtr points to.
+ */
 int32_t bson_value_to_int32(const void *valuePtr);
+
+/*
+ * @brief performs a quick validation of the BSON document pointed by document
+ *
+ * @details this function should be used to check if document is a valid BSON document.
+ *
+ * @param document the BSON document object that should be checked.
+ * @param fileSize the size of the BSON document.
+ * @returns 1 if document is likely a valid BSON document, 0 otherwise.
+ */
 int bson_check_validity(const void *document, unsigned int fileSize);
 
 #endif
