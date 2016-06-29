9483e63e0f7758b4f6b0df843f874a9a785ee74b
Davide Bettio <davide@uninstall.it>
2016-06-30 01:29:10 +0200

    Add poll() and singalfd() based event_loop_run implementation
    By using poll() and signalfd() we are not forced anymore to wait just for
    child processes events.
    signalfd() allows to keep track of SIGCHLD signal by monitoring a fd
    using poll() too.
    
    This change makes linux kernel version >= 2.6.27 mandatory.

--- src/eventloop.c
+++ src/eventloop.c
@@ -19,8 +19,11 @@
 
 #include "eventloop.h"
 
+#include <poll.h>
+#include <sys/signalfd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #include <stdio.h>
 #include <stdint.h>
@@ -64,6 +67,7 @@ int event_loop_run(EventLoop *loop)
 {
     main_event_loop->keep_running = 1;
 
+#ifdef SIMPLE_EVENT_LOOP
     while (main_event_loop->keep_running) {
         int status = 0;
         pid_t pid = waitpid(-1, &status, 0);
@@ -80,6 +84,39 @@ int event_loop_run(EventLoop *loop)
             }
         }
     }
+#else
+
+    sigset_t mask;
+    sigemptyset (&mask);
+    sigaddset (&mask, SIGCHLD);
+    sigprocmask (SIG_BLOCK, &mask, NULL);
+
+    int sfd = signalfd(-1, &mask, SFD_CLOEXEC);
+
+    struct pollfd fds[1];
+    fds[0].fd = sfd;
+    fds[0].events = POLLIN;
+    fds[0].revents = 0;
+
+    while (main_event_loop->keep_running) {
+        int status;
+        poll(fds, 1, -1);
+
+        pid_t pid = waitpid(-1, &status, 0);
+        if (pid > 0) {
+            for (int i = 0; i < main_event_loop->listeners_count; i++) {
+                EventListener *listener = (EventListener *) main_event_loop->listeners[i];
+                if (listener->type == EVENT_TYPE_CHILD) {
+                    ChildListener *child = (ChildListener *) listener;
+                    if (child->pid == pid) {
+                        event_child_handler_t handler = child->handler;
+                        handler(pid, listener->userdata);
+                    }
+                }
+            }
+        }
+    }
+#endif
 
     return EXIT_SUCCESS;
 }
