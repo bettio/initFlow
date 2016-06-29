fb063e95ea147b625e7b47b3f50288ae04630578
Davide Bettio <davide@uninstall.it>
2016-06-29 21:55:38 +0200

    eventloop.h: add doxygen documentation.

--- src/eventloop.h
+++ src/eventloop.h
@@ -1,3 +1,8 @@
+/**
+ * @file eventloop.h
+ * @brief simple event loop.
+ */
+
 /***************************************************************************
  *   Copyright 2016 by Davide Bettio <davide@uninstall.it>                 *
  *                                                                         *
@@ -27,9 +32,44 @@ struct _EventLoop typedef EventLoop;
 
 typedef void (*event_child_handler_t)(pid_t pid, void *userdata);
 
+/**
+ * @brief creates main event loop for this process.
+ *
+ * @details instantiates main event loop for this process, that will be globally available.
+ * This function returns a pointer to the main loop.
+ *
+ * @return a pointer to the main event loop.
+ */
 EventLoop *event_loop_create_main_loop();
+
+/**
+ * @brief runs the even loop pointed by loop.
+ *
+ * @details runs the event loop and it exits when event_loop_quit() is called.
+ *
+ * @param loop the event loop that will be runned.
+ * @return event loop exit status (e.g. EXIT_SUCCESS, EXIT_FAILURE or any user defined value).
+ */
 int event_loop_run(EventLoop *loop);
+
+/**
+ * @brief quits the event loop
+ *
+ * @details quits the event loop that is being runned.
+ */
 void event_loop_quit();
+
+/**
+ * @brief monitors a process pid for events.
+ *
+ * @details registers an event handler that will be called when a process exits.
+ *
+ * @param pid the process id that should be monitored.
+ * @param handler the function pointer that should be called.
+ * @param a pointer to some user defined data that will be passed to the handler when it gets called.
+ *
+ * returns 0 on success, otherwise -1 is returned.
+ */
 int event_loop_add_child(pid_t pid, event_child_handler_t handler, void *userdata);
 
 #endif
